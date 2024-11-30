67cfc6d6b9462463d132f615ac2dcf2a0797a078
webQbe <webcubesolutionslk@gmail.com>
2024-11-30 16:03:19 +0530

    Defined onGenerateSubmit()

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
 const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
 
+
+const onGenerateSubmit = (e) => {
+
+    e.preventDefault() // Prevents page reload on form submit
+
+    // Get input URL
+    const url = document.getElementById('url').value; 
+
+    // Get selected QR code size
+    const size = document.getElementById('size').value; 
+
+    console.log(url, size);
+
+}; 
+
+
+
 // Listen for submit event in form element
 form.addEventListener('submit', onGenerateSubmit);
-
-
