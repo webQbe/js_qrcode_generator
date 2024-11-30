c0cd68026ff75acdd178ce97e2604703ee8bbe2f
webQbe <webcubesolutionslk@gmail.com>
2024-11-30 15:55:07 +0530

    Added event listener to form

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@
 const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
 
+// Listen for submit event in form element
+form.addEventListener('submit', onGenerateSubmit);
+
 
